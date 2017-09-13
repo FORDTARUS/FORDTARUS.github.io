5a87f8c28127d30ddc55fd76fe8d2a60f1749e96
Monica Simmons <priceless5148@yahoo.com>
2017-09-12 20:55:44 -0400

    add var first name

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-document.getElementById('firstName')
-Cookies.set('firstName', 'Monica');
+//document.getElementById('firstName')
 
 // get to value of a input save it to cookie//
+var fName = document.getElementById('fname').value;
+// get to value of a input save it to cookie//
+Cookies.set('firstName', fName);
 
+//var fname = Cookies.get('firstName');
 
-var fname = Cookies.get('firstName');
-
-console.log(fname);
+console.log(fName);
